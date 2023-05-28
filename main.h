22935c6ef5e8140b18914c059b051354454873fc
Mennatallah Mohamed <menna.hesham2003@gmail.com>
2023-05-28 09:20:36 -0700

    .h and specifier.c

--- main.h
+++ main.h
@@ -1,13 +1,43 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
 #define BUFFER_SIZE 1024
 #define BUFFER_FLUSH -1
-/* printf.c tasks 0,1 */
+
+#define NULL_STRING "(NULL)"
+
+/* _puts.c */
+
+int _putchar(int c);
+int _puts(char *str);
+
+/**
+ * struct specifier - Structure token
+ * @specifier: token to be formated
+ * @f: Function
+ */
+typedef struct specifier
+{
+	char *specifier;
+	int(*f)(va_list, params_t);
+} specifier_t;
+
+
+/* printf functions */
+
+int print_char(valist ap, params_t *params);
+int print_percent(valist ap, params_t *params);
+int print_string(valist ap, params_t *params);
+
+
+/* printf.c */
 
 int _printf(const char *format, ...);
-int _putchar(char c);
 
 #endif
--- specifier.c
+++ specifier.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
-/*
+/**
  * _specifier - finds the format function
  * @s: the format string
- *
+ * Return: no of bytes to be printed
+ */
+
+int (*get_specifier(char *s))(va_list ap, params_t *params)
+{
+	specifier_t specifiers[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+	int n = 0;
+
+	while (specifier[n].specifier)
+	{
+		if (*s == specifiers[n].specifier[0])
+		{
+			return (specifiers[n].f);
+		}
+		n++;
+	}
+	return (NULL);
+}
